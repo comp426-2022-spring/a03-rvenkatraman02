0439d959df62e727971d17eecb93ecfcd9782e3a
rvenkatraman02 <rvenka@ad.unc.edu>
2022-03-01 01:05:11 -0500

    Finishing up coin function calls

--- server.js
+++ server.js
@@ -23,7 +23,43 @@ function coinFlip() {
     else if (flip >= 1) {
       return "heads";
     }
-  }
+}
+
+function coinFlips(flips) {
+    var results = [flips];
+    for (let i = 0; i < flips; i++) {
+        results[i] = coinFlip();
+    }
+    return results;
+}
+
+function countFlips(array) {
+    let heads_num = 0;
+    let tails_num = 0;
+    if(array.length > 1) {
+      for (let i = 0; i < array.length; i++) {
+        if (array[i] == 'tails') {
+          tails_num += 1;
+        }
+        else if (array[i] == 'heads') {
+          heads_num += 1;
+        }
+      }
+    }
+    else if (array.length == 1) {
+      if (array[0] == 'tails') tails_num += 1;
+      else if (array[0] == 'heads') heads_num += 1;
+    }
+    if (heads_num == 0) return {tails: tails_num};
+    else if (tails_num == 0) return {heads: heads_num};
+    else return {heads: heads_num, tails: tails_num};
+}
+
+function flipACoin(call) {
+    let toss = coinFlip();
+    if (toss.toUpperCase() == call.toUpperCase()) return {call: call, flip: toss, result: "win"};
+    else return {call: call, flip: toss, result: "lose"} ;
+}
 
 // Default response for any other request
 app.use(function(req,res){
@@ -41,15 +77,12 @@ app.get('/app/', (req,res) => {
 
 // Endpoint definition
 app.get('/app/flips/:number', (req, res) => {
-    const flips = manyflips(req.params.number)
-	//Some
-	//expressions
-	//go
-	//here
+    var array = coinFlips(req.params["number"])
+    res.status(200).json({"raw": array, "summary" : {"tails" : countFlips(array).tails, "heads" : countFlips(array).heads}})
 });
 
-app.get('/app/flip', (req,res) => {
-    res.status(200).json({'flip' : coinFlip()})
+app.get('/app/flip/call/:this_call', (req,res) => {
+    res.status(200).json(flipACoin(req.params["this_call"]))
 })
 
 app.use(function(req,res) {
